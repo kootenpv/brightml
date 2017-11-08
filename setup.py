df4cf8eee0fa750a26b53df3ef3e9ad2abb70a8c
Pascal van Kooten <kootenpv@gmail.com>
2017-11-08 10:20:42 +0100

    added missing dep, removed old requirements txt (solves #6)

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ import os
 import sys
 import codecs
 
+
 from setuptools import setup, find_packages
+
+a = "this is a very long string fjjjjjjor example ifnd padf asdfasdf adsf asdfas dfjjjjjjjjj aadsfasmmmmmmmmm "
+
 
 try:
     # Python 3
@@ -31,7 +35,8 @@ required = [
     "python-xlib",
     "Pillow",
     "simplejson",
-    "pyinotify"
+    "pyinotify",
+    "watchdog"
 ]
 
 extras_require = {
